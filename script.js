a1de7fc6dab602fbac393d68716df05f0cad8c87
AL SABAB <sabab015@gmail.com>
2022-11-10 16:50:49 +0600

    random score via api shown

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
-fetch("https://api.cricapi.com/v1/currentMatches?apikey=8992a6e5-a950-4f5b-b60f-c2fe7826b80e&offset=0")
+fetch("https://api.cricapi.com/v1/cricScore?apikey=8992a6e5-a950-4f5b-b60f-c2fe7826b80e")
     .then((data) => {
         return data.json();
     })
     .then((objectData) => {
-        console.log(objectData.data[0].name);
-        let tableData = "";
+        //console.log(objectData.data[0].name);
+        let scoreData1 = "";
+        let scoreData2 = "";
         objectData.data.map((values) => {
-            tableData += ` <tr>
-            <td>${values.name.r}</td>
-            <td>${values.matchType}</td>
-            <td>${values.date}</td>
-            <td>${values.teams}</td>
-            <td>${values.status}</td>
-        </tr>`;
-            console.log(tableData);
+            scoreData1 = `
+            <img src="${values.t1img}"/>
+            <p>${values.t1}</p>
+            <p>${values.t1s}</p>
+            `;
+            console.log(scoreData1);
+
+            scoreData2 = `
+            <img src="${values.t2img}"/>
+            <p>${values.t2}</p>
+            <p>${values.t2s}</p>
+            `;
+            console.log(scoreData2);
         });
-        document.getElementById("table_body").innerHTML = tableData;
+        document.getElementById("F_Team").innerHTML = scoreData1;
+        document.getElementById("L_Team").innerHTML = scoreData2;
     })
